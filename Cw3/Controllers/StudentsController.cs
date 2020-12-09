852ee41c92b39f57430526a45f39f9edca4f9f78
Misha Kurch <s13648@pjwstk.edu.pl>
2020-12-10 00:56:23 +0100

    Zadanie 5

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -6,19 +6,10 @@ namespace Cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet("{id}")]
-        public IActionResult GeStudents(int id)
+        [HttpGet]
+        public IActionResult GeStudents(string orderBy)
         {
-            if (id == 1)
-            {
-                return Ok("Kowalski");
-            }
-            else if(id == 2)
-            {
-                return Ok("Malewski");
-            }
-
-            return NotFound("Nie znaleziono studenta");
+            return Ok($"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}");
         }
     }
 }

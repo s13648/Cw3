a700091787f18e73e0e6a87a0887f1b5b361fceb
Misha Kurch <s13648@pjwstk.edu.pl>
2020-12-10 00:24:03 +0100

    Initial commit

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GeStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if(id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+    }
+}

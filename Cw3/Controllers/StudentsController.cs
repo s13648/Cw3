7ca7f2d544dcf0a133475873f48d5fbe540dfafd
Misha Kurch <s13648@pjwstk.edu.pl>
2020-12-10 01:01:13 +0100

    Zadanie 6

--- Cw3/Controllers/StudentsController.cs
+++ Cw3/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cw3.Controllers
 {
@@ -10,6 +12,13 @@ namespace Cw3.Controllers
         public IActionResult GeStudents(string orderBy)
         {
             return Ok($"Kowalski, Malewski, Andrzejewski sortowanie = {orderBy}");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"{new Random().Next(1, 20000)}";
+            return Ok(student);
         }
     }
 }
